014ca22e4d398f095b4d0d1b3334acab0a1e77df
Salvio Furbino <salviof@gmail.com>
2021-01-13 19:49:29 -0300

    up

---
+++ servicos/varnish/servicoUpdate.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo "SCRIPT REMOTO ATUALIZAÇÃO HOSPEDAGEM WORDPRESS INICIADO"
+
+mkdir /var/coletivoOpenSource/varnish/ -p
+
+if [ ! -d '/var/coletivoOpenSource/varnish/' ]
+then 
+   echo "A pasta /var/coletivoOpenSource/varnish/ não pôde ser criada"
+   exit 1
+fi
+
+if [ -d '/var/coletivoOpenSource/varnishInstall' ]
+then 
+echo "a pasta de arquivo temporaria foi encontrada no sistema removendo antes do clone"
+rm /var/coletivoOpenSource/wordpressInstall -rf 
+else
+echo "iniciando clone da pasta de instalação"
+fi
+cd /var/coletivoOpenSource/
+git clone https://github.com/salviof/infraColetivoOpenSource.git varnishInstall
+
+cd /var/coletivoOpenSource/
+
+
+cp /var/coletivoOpenSource/varnishInstall/servicos/varnish/docker-compose.yml /var/coletivoOpenSource/varnish/ -f
+
+if [ ! -f /var/coletivoOpenSource/varnishInstall/servicos/varnish/.env ]  then
+    echo "Arquivo env não encontrado, implantando arquivo env padrão "
+    cp /var/coletivoOpenSource/varnishInstall/servicos/varnish/envExemplo /var/coletivoOpenSource/varnish/.env -f
+fi
+
+
+
+
+
+cd /var/coletivoOpenSource/varnishInstall/servicos/varnish/builderDockerfileVarnish
+./buildimagem.sh
+
+if [ -d '/var/coletivoOpenSource/wordpressInstall' ]
+then 
+rm /var/coletivoOpenSource/varnishInstall -rf
+else 
+echo "erro localizando pasta de instalação do contexto"
+exit 1
+fi
+
+cd /var/coletivoOpenSource/wordpress/
+echo " - Parabéns para mim,"
+echo " - o SCRIPT BASH! "
+echo "O SERVIDOR foi atualizado com sucesso!"
+
+
